d8331b564126b8db5fb209aefef8403ec50ed89d
luks22 <luksilva1796@gamil.com>
2020-04-12 16:17:01 -0300

    removida a pasta meteriologia

--- meteriologia/src/main/java/br/usjt/meteriologia/repository/MeteriologiaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.usjt.meteriologia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.usjt.meteriologia.model.Meteriologia;
-
-public interface MeteriologiaRepository extends JpaRepository<Meteriologia,Long>{
-
-}
--- meteriologia/src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-insert into usuario (id, login, senha) values (1, 'admin', 'admin');
